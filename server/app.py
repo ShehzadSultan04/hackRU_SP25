605ea4cb18196e3c644f674c63d9c39a460d7182
Darshan <ldarsh04@gmail.com>
2025-02-01 16:06:47 -0500

    basic login structure

--- server/app.py
+++ server/app.py
@@ -1,12 +1,48 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect
+import passwords
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient(passwords.passwords["MongoURI"])
+db = client["Secretary"]
+users = db["Users"]
+
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return"<h1><a href=\"/signup\">signup</a></h1><h1><a href=\"/login\">login</a></h1>"
 
-@app.route("/about")
-def aboute():
-    return "<h1>About Us</h1>"
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    else:
+        username = request.form["username"]
+        password = request.form["password"]
+        
+        results = users.find({"$and": [{"username": username}, {"password": password}]}).to_list()
+        
+        if len(results) == 1:
+            return "<h1>Welcome " + username + "!</h1>"
+        else:
+            return "<h1>Invalid username or password</h1>"
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "GET":
+        return render_template("signup.html")
+    else:
+        username = request.form["username"]
+        password = request.form["password"]
+        
+        results = users.find({"username": username}).to_list()
+         
+        # #check if user already exists
+        if len(results) == 0:
+            users.insert_one({"username": username, "password": password})
+            return redirect("/")
+        else:
+            return "<h3>User already exists. Login instead</h3>"
